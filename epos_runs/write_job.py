24a978b0d62a0bb1ad9a9bd7b83782e857fde621
Katharine Mulrey <kmulrey@Katharines-MacBook-Pro.local>
2020-02-05 19:17:20 +0100

    .

--- epos_runs/write_job.py
+++ epos_runs/write_job.py
@@ -20,8 +20,8 @@ def write_file(event, azimuth, zenith, energy, seed,type):
 
     outfile.write('#! /bin/bash\n')
     outfile.write('#SBATCH --time=7-00:00:00\n')
-    outfile.write('#SBATCH --output {0}/run/output/'+event+'_coreas_'+part_id+'-%j\n'.format(base_dir))
-    outfile.write('#SBATCH --error {0}/run/output/'+event+'_coreas_'+part_id+'-ERROR-%j\n'.format(base_dir))
+    outfile.write('#SBATCH --output {0}/run/output/{1}_coreas_{2}-%j\n'.format(base_dir,event,part_id))
+    outfile.write('#SBATCH --error {0}/run/output/{1}_coreas_{2}-ERROR-%j\n'.format(base_dir,event,part_id))
 
 
 
