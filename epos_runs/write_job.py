8e29a22dc82540223f1eaad49c174d380768ec31
Katharine Mulrey <kmulrey@Katharines-MacBook-Pro.local>
2020-02-05 19:15:25 +0100

    .

--- epos_runs/write_job.py
+++ epos_runs/write_job.py
@@ -29,18 +29,28 @@ def write_file(event, azimuth, zenith, energy, seed,type):
     outfile.write('export RUNNR=`printf "%06d" $SLURM_ARRAY_TASK_ID`\n')
     outfile.write('export FLUPRO=/vol/optcoma/cr-simulations/fluka64\n')
     outfile.write('cd {0}/run/'.format(base_dir))
-    outfile.write('mkdir -p {0}/events/'+event+'/coreas/'+type+'/steering/\n'.format(base_dir))
-    outfile.write('rm -rf /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR\n')
-    outfile.write('mkdir -p /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR\n')
-    outfile.write('python /vol/optcoma/pycrtools/src/PyCRTools/extras/geninp.py --atmosphere --atmfile=/vol/astro7/lofar/sim/pipeline/atmosphere_files/ATMOSPHERE_'+event+'.DAT -r $RUNNR -s {0} -u {1} -a {2} -z {3} -t '.format(seed,energy,azimuth,zenith)+part_id+' -d /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR/ > /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR/RUN$RUNNR.inp\n')
+    outfile.write('mkdir -p {0}/events/{1}/coreas/{2}/steering/\n'.format(base_dir,event,type))
+    outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
+    outfile.write('mkdir -p /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
+    outfile.write('python /vol/optcoma/pycrtools/src/PyCRTools/extras/geninp.py --atmosphere --atmfile=/vol/astro7/lofar/sim/pipeline/atmosphere_files/ATMOSPHERE_{4}.DAT -r $RUNNR -s {0} -u {1} -a {2} -z {3} -t {5} -d /scratch/kmulrey/{4}/{5}/$RUNNR/ > /scratch/kmulrey/{4}/{5}/$RUNNR/RUN$RUNNR.inp\n'.format(seed,energy,azimuth,zenith,event,part_id))
 
-
-
-
-
-
+    outfile.write('cp /vol/astro7/lofar/sim/pipeline/run/SIM.reas /scratch/kmulrey/{0}/{1}/$RUNNR/SIM$RUNNR.reas\n'.format(event,part_id))
+    outfile.write('cp /vol/astro7/lofar/sim/pipeline/run/SIM{0}.list /scratch/kmulrey/{0}/{1}/$RUNNR/SIM$RUNNR.list\n'.format(event,part_id))
+    outfile.write('cd /vol/optcoma/cr-simulations/corsika_production_epos/run\n')
+    outfile.write('./corsika77100Linux_EPOS_urqmd_thin_conex_coreas < //scratch/kmulrey/{0}/{1}/$RUNNR/RUN$RUNNR.inp\n'.format(event,part_id))
+    outfile.write('cd /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id)
+    outfile.write('mv RUN$RUNNR.inp {0}/events/{1}/coreas/{2}/steering/RUN$RUNNR.inp\n'.format(base_dir,event,type))
+    outfile.write('mv *.long {0}events/{1}/coreas/{2}/\n'.format(base_dir,event,type))
+    outfile.write('/vol/optcoma/pycrtools/LORA_simulation/DAT2txt DAT$RUNNR DAT$RUNNR.tmp\n')
+    outfile.write('/vol/optcoma/pycrtools/LORA_simulation/LORA_simulation DAT$RUNNR.tmp DAT$RUNNR.lora\n')
+    outfile.write('rm DAT$RUNNR.tmp\n')
+    outfile.write('mv SIM$RUNNR.reas {0}events/{1}/coreas/{2}/steering/SIM$RUNNR.reas\n'.format(base_dir,event,type))
+    outfile.write('mv SIM$RUNNR.list {0}events/{1}/coreas/{2}/steering/SIM$RUNNR.list\n'.format(base_dir,event,type))
+    outfile.write('cp -r * {0}events/{1}/coreas/{2}/\n'.format(base_dir,event,type))
+    outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR/*\n'.format(event,part_id))
 
     outfile.close()
+            
 
 
 azimuth= -149.856203773
@@ -49,22 +59,3 @@ energy=166070995.766
 seed=25056
 
 write_file(str(60309606),azimuth, zenith, energy, seed,'proton')
-
-'''
-
-python /vol/optcoma/pycrtools/src/PyCRTools/extras/geninp.py --atmosphere --atmfile=/vol/astro7/lofar/sim/pipeline/atmosphere_files/ATMOSPHERE_188735056.DAT -r $RUNNR -s 25056 -u 166070995.766 -a -149.856203773 -z 25.1606454355 -t 5626 -d /scratch/crsim_pipeline/188735056/5626/$RUNNR/ > /scratch/crsim_pipeline/188735056/5626/$RUNNR/RUN$RUNNR.inp
-cp /vol/astro7/lofar/sim/pipeline/run/SIM.reas /scratch/crsim_pipeline/188735056/5626/$RUNNR/SIM$RUNNR.reas
-cp /vol/astro7/lofar/sim/pipeline/run/SIM188735056.list /scratch/crsim_pipeline/188735056/5626/$RUNNR/SIM$RUNNR.list
-cd /vol/optcoma/cr-simulations/corsika_production/run
-./corsika77100Linux_QGSII_fluka_thin_coreas < //scratch/crsim_pipeline/188735056/5626/$RUNNR/RUN$RUNNR.inp
-cd /scratch/crsim_pipeline/188735056/5626/$RUNNR
-mv RUN$RUNNR.inp /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/steering/RUN$RUNNR.inp
-mv *.long /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/
-# /vol/optcoma/cr-simulations/LORAtools/DAT2txt DAT$RUNNR DAT$RUNNR.tmp
-# /vol/optcoma/cr-simulations/LORAtools/LORA_simulation DAT$RUNNR.tmp DAT$RUNNR.lora
-# rm DAT$RUNNR.tmp
-mv SIM$RUNNR.reas /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/steering/SIM$RUNNR.reas
-mv SIM$RUNNR.list /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/steering/SIM$RUNNR.list
-cp -r * /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/
-rm -rf /scratch/crsim_pipeline/188735056/5626/$RUNNR/*
-'''
