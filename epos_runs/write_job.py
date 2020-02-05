73aca15ef233c7d28470fd391c7dbd87c4bf3e03
Katharine Mulrey <kmulrey@Katharines-MacBook-Pro.local>
2020-02-05 18:50:05 +0100

    .

--- epos_runs/write_job.py
+++ epos_runs/write_job.py
@@ -3,8 +3,10 @@ iron_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_syst
 
 base_dir='/vol/astro7/lofar/sim/pipeline_epos/'
 
-def write_file(event,type):
+def write_file(event, azimuth, zenith, energy, seed,type):
 
+
+    
     part_id=''
     if type=='proton':
         part_id='14'
@@ -28,7 +30,9 @@ def write_file(event,type):
     outfile.write('export FLUPRO=/vol/optcoma/cr-simulations/fluka64')
     outfile.write('cd {0}/run/'.format(base_dir))
     outfile.write('mkdir -p {0}/events/'+event+'/coreas/'+type+'/steering/'.format(base_dir))
-
+    outfile.write('rm -rf /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR')
+    outfile.write('mkdir -p /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR')
+    outfile.write('python /vol/optcoma/pycrtools/src/PyCRTools/extras/geninp.py --atmosphere --atmfile=/vol/astro7/lofar/sim/pipeline/atmosphere_files/ATMOSPHERE_'+event+'.DAT -r $RUNNR -s '+seed+' -u '+energy+' -a '+azimuth+' -z '+zenith+' -t '+part_id+' -d /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR/ > /scratch/kmulrey/'+event+'/'+part_id+'/$RUNNR/RUN$RUNNR.inp')
 
 
 
@@ -39,16 +43,15 @@ def write_file(event,type):
     outfile.close()
 
 
+azimuth= -149.856203773
+zenith=25.1606454355
+energy=166070995.766
+seed=25056
 
-write_file(str(60309606),'proton')
+write_file(str(60309606),azimuth, zenith, energy, seed,'proton')
 
 '''
 
-
-
-mkdir -p /vol/astro7/lofar/sim/pipeline/events/188735056/2/coreas//iron/steering/
-rm -rf /scratch/crsim_pipeline/188735056/5626/$RUNNR
-mkdir -p /scratch/crsim_pipeline/188735056/5626/$RUNNR
 python /vol/optcoma/pycrtools/src/PyCRTools/extras/geninp.py --atmosphere --atmfile=/vol/astro7/lofar/sim/pipeline/atmosphere_files/ATMOSPHERE_188735056.DAT -r $RUNNR -s 25056 -u 166070995.766 -a -149.856203773 -z 25.1606454355 -t 5626 -d /scratch/crsim_pipeline/188735056/5626/$RUNNR/ > /scratch/crsim_pipeline/188735056/5626/$RUNNR/RUN$RUNNR.inp
 cp /vol/astro7/lofar/sim/pipeline/run/SIM.reas /scratch/crsim_pipeline/188735056/5626/$RUNNR/SIM$RUNNR.reas
 cp /vol/astro7/lofar/sim/pipeline/run/SIM188735056.list /scratch/crsim_pipeline/188735056/5626/$RUNNR/SIM$RUNNR.list
