34353aaf1ff5322f823354529eb638e468da3335
Katharine Mulrey <kmulrey@Katharines-MacBook-Pro.local>
2020-02-05 19:36:13 +0100

    with variable input to write function

--- epos_runs/write_job.py
+++ epos_runs/write_job.py
@@ -3,7 +3,7 @@ iron_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_syst
 
 base_dir='/vol/astro7/lofar/sim/pipeline_epos/'
 
-def write_file(event, azimuth, zenith, energy, seed,type):
+def write_file(event, azimuth, zenith, energy, seed, type):
 
 
     
@@ -23,12 +23,12 @@ def write_file(event, azimuth, zenith, energy, seed,type):
     outfile.write('#SBATCH --output {0}/run/output/{1}_coreas_{2}-%j\n'.format(base_dir,event,part_id))
     outfile.write('#SBATCH --error {0}/run/output/{1}_coreas_{2}-ERROR-%j\n'.format(base_dir,event,part_id))
 
-
+    outfile.write('umask 002\n')
 
     outfile.write('. /vol/optcoma/geant4_9.6_install/share/Geant4-9.6.4/geant4make/geant4make.sh\n')
     outfile.write('export RUNNR=`printf "%06d" $SLURM_ARRAY_TASK_ID`\n')
     outfile.write('export FLUPRO=/vol/optcoma/cr-simulations/fluka64\n')
-    outfile.write('cd {0}/run/'.format(base_dir))
+    outfile.write('cd {0}/run/\n'.format(base_dir))
     outfile.write('mkdir -p {0}/events/{1}/coreas/{2}/steering/\n'.format(base_dir,event,type))
     outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
     outfile.write('mkdir -p /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
@@ -50,12 +50,21 @@ def write_file(event, azimuth, zenith, energy, seed,type):
     outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR/*\n'.format(event,part_id))
 
     outfile.close()
-            
 
 
-azimuth= -149.856203773
-zenith=25.1606454355
-energy=166070995.766
-seed=25056
 
-write_file(str(60309606),azimuth, zenith, energy, seed,'proton')
+event=[60409606,61271909,63246671,65490891,65214973,64960703,70904490,80495081,85083852,84432712,83467151]
+azimuth=[57.8777037378,5.62278563,24.5338416226,85.323975652,-45.2948503091,-11.61947731,25.876497776,-170.239430922,130.84941074,14.554336601,-32.732258602]
+zenith=[28.7984121752,36.3926767814,17.6159726846,28.5091835026,38.2803974379,35.5384552615,14.4052883375,40.6923782667,20.8766959694,39.5359400205,47.2151295936]
+energy=[159421449.975,656804646.049,470693726.644,102292495.459,325078049.841,151927036.517,127390466.343,319912958.115,101648514.077,273404353.855,201546777.962]
+seed=[43356,6726,53394,72474,38098,15078,60740,25706,51352,37712,91526]
+
+
+
+#azimuth= -149.856203773
+#zenith=25.1606454355
+#energy=166070995.766
+#seed=25056
+for i in np.arange(len(event)):
+    write_file(str(int(events[i])),azimuth[i], zenith[i], energy[i], seed[i],'proton')
+    write_file(str(int(events[i])),azimuth[i], zenith[i], energy[i], seed[i],'iron')
