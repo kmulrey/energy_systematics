f0363dd141935dfd5da7acb2f4c0d5cf59e1131b
Katie <kmurley@gmail.com>
2020-02-08 13:12:07 +0100

    .

--- xmax_pipeline/run_cr_xmax.py
+++ xmax_pipeline/run_cr_xmax.py
@@ -45,7 +45,7 @@ COLLECT_DIR='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/filtered
 def run_event(event):
 
     try:
-        runCommand =    'python cr_xmaxfit.py --event={0} --lorafile-suffix=_GeVfix --datadir={1} --simulationdir={2} --iteration=0  --outputdir-radio-only={3} --logdir={4} --filtdir={5} --writedir={6} --collectdir={7}'.format(event,DATA_DIR,SIMULATION_DIR,OUTPUT_DIR_RADIO_ONLY,LOG_DIR,NEWSIM_PATH,WRITE_FILT,COLLECT_DIR)
+        runCommand =    'python cr_xmaxfit.py --event={0} --lorafile-suffix=_GeVfix --datadir={1} --simulationdir={2} --iteration=0  --outputdir-radio-only={3} --logdir={4} --filtdir={5} --writedir={6} --collectdir={7}'.format(int(event),DATA_DIR,SIMULATION_DIR,OUTPUT_DIR_RADIO_ONLY,LOG_DIR,NEWSIM_PATH,WRITE_FILT,COLLECT_DIR)
     
         print 'Running command: %s' % runCommand
         #process = subprocess.Popen([runCommand], shell=True)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -58,7 +58,7 @@ def run_event(event):
     
     
     except:
-        print 'issue running {0}'.format(event)
+        print 'issue running {0}'.format(int(event))
     
     
     
@@ -70,10 +70,10 @@ event=int(events[11])
 
 print '--------> event {0}'.format(event)
 
-use=[int(events[13]),int(events[14]),int(events[15]),int(events[16])]
+#use=[int(events[13]),int(events[14]),int(events[15]),int(events[16])]
 
-p = Pool(2)
-p.map(run_event,use)
+p = Pool(12)
+p.map(run_events,events)
 #run_event(event)
 
 
