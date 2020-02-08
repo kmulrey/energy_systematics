94e9d16ceb9337e65ef666a297191b5526fe914b
Katie <kmurley@gmail.com>
2020-02-08 12:46:27 +0100

    .

--- xmax_pipeline/run_cr_xmax.py
+++ xmax_pipeline/run_cr_xmax.py
@@ -30,10 +30,22 @@ def waitAndHandleErrors(process, name):
         print '{0} finished normally.'.format(name)
 
 
+BASE_PATH='/vol/astro7/lofar/sim/pipeline/'
+RESULTS_PATH='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/results/'
+NEWSIM_PATH='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/'
+DATA_DIR=BASE_PATH+'events/'
+SIMULATION_DIR=BASE_PATH+'run/'
+OUTPUT_DIR_RADIO_ONLY=RESULTS_PATH+'production_analysis_radio_only_thetaPLUS5/'
+LOG_DIR=RESULTS_PATH+'log/'
+WRITE_FILT='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/events_thetaPLUS5/'
+COLLECT_DIR='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/filtered_thetaPLUS5/'
+
+
+
 def run_event(event):
 
 
-    runCommand =    'python cr_xmaxfit.py --event={0} --lorafile-suffix=_GeVfix --datadir=$DATA_DIR --simulationdir=$SIMULATION_DIR --iteration=0  --outputdir-radio-only=$OUTPUT_DIR_RADIO_ONLY --mcvsmcdir=$MCVSMC_DIR --logdir=$LOG_DIR --filtdir=$NEWSIM_PATH --writedir=$WRITE_FILT --collectdir=$COLLECT_DIR'.format(event)
+    runCommand =    'python cr_xmaxfit.py --event={0} --lorafile-suffix=_GeVfix --datadir={1} --simulationdir={2} --iteration=0  --outputdir-radio-only={3} --logdir={4} --filtdir={5} --writedir={6} --collectdir={8}'.format(event,DATA_DIR,SIMULATION_DIR,OUTPUT_DIR_RADIO_ONLY,LOG_DIR,NEWSIM_PATH,WRITE_FILT,COLLECT_DIR)
     
     print 'Running command: %s' % runCommand
     #process = subprocess.Popen([runCommand], shell=True)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
