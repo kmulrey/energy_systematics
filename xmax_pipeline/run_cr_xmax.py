ca47334f477e72fe9852457e0b666b19131dd1b4
Katie <kmurley@gmail.com>
2020-02-11 17:43:25 +0100

    .

--- xmax_pipeline/run_cr_xmax.py
+++ xmax_pipeline/run_cr_xmax.py
@@ -69,7 +69,7 @@ def run_event(event):
 #event=int(events[11])
 #event=196796518
 
-print '--------> event {0}'.format(event)
+#print '--------> event {0}'.format(event)
 
 #use=[int(events[13]),int(events[14]),int(events[15]),int(events[16])]
 p = Pool(12)
