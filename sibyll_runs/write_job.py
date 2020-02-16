9d785246a9f38705a0fac95a282ed979f54c9f14
Katie <kmurley@gmail.com>
2020-02-16 20:02:48 +0100

    .

--- sibyll_runs/write_job.py
+++ sibyll_runs/write_job.py
@@ -24,8 +24,8 @@ def write_file(event, azimuth, zenith, energy, seed, type):
 
     outfile.write('#! /bin/bash\n')
     outfile.write('#SBATCH --time=7-00:00:00\n')
-    outfile.write('#SBATCH --output {0}/run/output/{1}_coreas_{2}-%j\n'.format(base_dir,event,part_id))
-    outfile.write('#SBATCH --error {0}/run/output/{1}_coreas_{2}-ERROR-%j\n'.format(base_dir,event,part_id))
+    #outfile.write('#SBATCH --output {0}/run/output/{1}_coreas_{2}-%j\n'.format(base_dir,event,part_id))
+    #outfile.write('#SBATCH --error {0}/run/output/{1}_coreas_{2}-ERROR-%j\n'.format(base_dir,event,part_id))
 
     outfile.write('umask 002\n')
     outfile.write('use geant\n')
