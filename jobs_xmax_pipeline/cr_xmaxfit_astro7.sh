5bb898738371780fb8ae6df1e34ba8f3a046cbbb
Katie <kmurley@gmail.com>
2020-02-27 15:11:15 +0100

    .

--- jobs_xmax_pipeline/cr_xmaxfit_astro7.sh
+++ jobs_xmax_pipeline/cr_xmaxfit_astro7.sh
@@ -11,16 +11,16 @@ export LD_LIBRARY_PATH=$LOFARSOFT/release/lib:$LD_LIBRARY_PATH
 
 #EVENT_ID=$(awk "NR==$SLURM_ARRAY_TASK_ID" $HOME/cr_physics_new)
 
-EVENT_ID=$SLURM_ARRAY_TASK_ID
+#EVENT_ID=$SLURM_ARRAY_TASK_ID
 
-echo $EVENT_ID
+echo $SLURM_ARRAY_TASK_ID
 
 
 
 # Set permissions on resulting files
 umask 002
 
-echo $EVENT_ID
+#echo $EVENT_ID
 echo $PYCRTOOLS
 
 cd /vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/xmax_pipeline
@@ -28,4 +28,4 @@ cd /vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics
 
 #python cr_xmaxfit.py --event=$EVENT_ID --lorafile-suffix=_GeVfix --datadir=$DATA_DIR --simulationdir=$SIMULATION_DIR --iteration=0  --outputdir-radio-only=$OUTPUT_DIR_RADIO_ONLY --mcvsmcdir=$MCVSMC_DIR --logdir=$LOG_DIR --filtdir=$NEWSIM_PATH --writedir=$WRITE_FILT --collectdir=$COLLECT_DIR
 
-python run_cr_xmax.py -i $EVENT_ID
+python run_cr_xmax.py -i $SLURM_ARRAY_TASK_ID
