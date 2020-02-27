940eade405e5701eceb2e51c459b65b3f7fc7afb
Katie <kmurley@gmail.com>
2020-02-27 15:04:55 +0100

    added back in bash array submission

--- jobs_xmax_pipeline/cr_xmaxfit_astro7.sh
+++ jobs_xmax_pipeline/cr_xmaxfit_astro7.sh
@@ -9,8 +9,9 @@ export LOFARSOFT=/vol/optcoma/pycrtools
 export PYTHONPATH=$LOFARSOFT/release/lib/python:$PYTHONPATH
 export LD_LIBRARY_PATH=$LOFARSOFT/release/lib:$LD_LIBRARY_PATH
 
-EVENT_ID=$(awk "NR==$SLURM_ARRAY_TASK_ID" $HOME/cr_physics_new)
+#EVENT_ID=$(awk "NR==$SLURM_ARRAY_TASK_ID" $HOME/cr_physics_new)
 
+EVENT_ID=$SLURM_ARRAY_TASK_ID
 
 echo $EVENT_ID
 
@@ -25,4 +26,6 @@ echo $PYCRTOOLS
 cd /vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/xmax_pipeline
 
 
-python cr_xmaxfit.py --event=$EVENT_ID --lorafile-suffix=_GeVfix --datadir=$DATA_DIR --simulationdir=$SIMULATION_DIR --iteration=0  --outputdir-radio-only=$OUTPUT_DIR_RADIO_ONLY --mcvsmcdir=$MCVSMC_DIR --logdir=$LOG_DIR --filtdir=$NEWSIM_PATH --writedir=$WRITE_FILT --collectdir=$COLLECT_DIR
+#python cr_xmaxfit.py --event=$EVENT_ID --lorafile-suffix=_GeVfix --datadir=$DATA_DIR --simulationdir=$SIMULATION_DIR --iteration=0  --outputdir-radio-only=$OUTPUT_DIR_RADIO_ONLY --mcvsmcdir=$MCVSMC_DIR --logdir=$LOG_DIR --filtdir=$NEWSIM_PATH --writedir=$WRITE_FILT --collectdir=$COLLECT_DIR
+
+python run_cr_xmax.py -i $EVENT_ID
