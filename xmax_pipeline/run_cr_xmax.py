83ad018e86db041316ab4869e1007a57124bb95e
Katie <kmurley@gmail.com>
2020-02-27 18:27:49 +0100

    .

--- xmax_pipeline/run_cr_xmax.py
+++ xmax_pipeline/run_cr_xmax.py
@@ -49,11 +49,11 @@ RESULTS_PATH='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/results
 NEWSIM_PATH='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/'
 DATA_DIR=BASE_PATH+'events/'
 SIMULATION_DIR=BASE_PATH+'run/'
-OUTPUT_DIR_RADIO_ONLY=RESULTS_PATH+'production_analysis_radio_only_cal_var1/'
+OUTPUT_DIR_RADIO_ONLY=RESULTS_PATH+'production_analysis_radio_only_cal_var2/'
 LOG_DIR=RESULTS_PATH+'log/'
 WRITE_FILT='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/events/'#_thetaMINUS1/
 COLLECT_DIR='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/filtered/'#_thetaMINUS1/
-LORA_DIR='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/data_cal_var1/'
+LORA_DIR='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/data_cal_var2/'
 
 
 def run_event(event):
