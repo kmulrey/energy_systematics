ac10b9e3a74423cc1e7484b738bbae3589b2b373
Katie <kmurley@gmail.com>
2020-03-10 17:33:27 +0100

    .

--- xmax_pipeline/run_cr_xmax.py
+++ xmax_pipeline/run_cr_xmax.py
@@ -82,12 +82,12 @@ def run_event(event):
     
 ########################################################
 
-#event=196796518
+event=64960703
 
 
-p = Pool(12)
-p.map(run_event,events)
-#run_event(event)
+#p = Pool(12)
+#p.map(run_event,events)
+run_event(event)
 
 
 
