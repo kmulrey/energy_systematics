a4068c4ac32bb1fd73588a0d57a893981fbc4dcb
Katie <kmurley@gmail.com>
2020-03-16 11:19:30 +0100

    .

--- xmax_pipeline/filterjobs_perevent.py
+++ xmax_pipeline/filterjobs_perevent.py
@@ -88,7 +88,7 @@ for d in dirs:
         #check if all files are ready
         
         
-        '''
+        
         print '\n\n'
         print os.path.isdir(d+"/SIM{0}_coreas".format(str(showerno).zfill(6)))
         print os.path.isfile(d+"/DAT{0}{1}.lora".format(str(showerno).zfill(6), lorafile_suffix)),d+"/DAT{0}{1}.lora".format(str(showerno).zfill(6), lorafile_suffix)
@@ -97,7 +97,7 @@ for d in dirs:
         print os.path.isfile(d+"/steering/RUN{0}.inp".format(str(showerno).zfill(6)))
         print os.path.isfile(d+"/steering/SIM{0}.list".format(str(showerno).zfill(6)))
         print '\n\n'
-        '''
+        
         if (os.path.isdir(d+"/SIM{0}_coreas".format(str(showerno).zfill(6))) and
             os.path.isfile(d+"/DAT{0}{1}.lora".format(str(showerno).zfill(6), lorafile_suffix)) and
             (os.stat(d+"/DAT{0}.long".format(str(showerno).zfill(6))).st_size>0) and
