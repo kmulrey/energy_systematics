f2dedfc4e5df28a040c6aa2d1d964010266ef281
Katie <kmurley@gmail.com>
2020-03-16 20:13:08 +0100

    .

--- reas_runs/write_job_part2.py
+++ reas_runs/write_job_part2.py
@@ -3,7 +3,7 @@ import numpy as np
 proton_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/reas_runs/jobs_proton_part2/'
 iron_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/reas_runs/jobs_iron_part2/'
 
-base_dir='/vol/astro7/lofar/sim/pipeline_reas_bug/'
+base_dir='/vol/astro7/lofar/sim/pipeline_reas/'
 
 def write_file(event, type):
 
