af35b687f15332ecd5cb57b2760f8b5b71a817bc
Katie <kmurley@gmail.com>
2020-04-03 12:48:29 +0200

    .

--- xmax_pipeline/cr_xmaxfit.py
+++ xmax_pipeline/cr_xmaxfit.py
@@ -128,13 +128,13 @@ if options.debug_lofar_pulse:
 #################################################################
 #################################################################
 
-
+'''
 runCommand = 'python -u '+scripts_directory+'/filterjobs_perevent.py --eventid={0} --force-reprocess --writedir={3} --datadir={1} {2}'.format(eventid, datadir, additional_flags,writedir)#logfile
 print 'Running command: %s' % runCommand
 process = subprocess.Popen([runCommand], shell=True)#, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 waitAndHandleErrors(process, 'filterjobs_perevent.py')
 
-
+'''
 
 
 
@@ -145,7 +145,7 @@ waitAndHandleErrors(process, 'filterjobs_perevent.py')
 #################################################################
 #################################################################
 #################################################################
-'''
+
 collect_output=collect_outputdir
 print '___________________'
 print collect_outputdir
@@ -163,7 +163,7 @@ waitAndHandleErrors(process, 'collectfiles_perevent.py')
 #print 'Running command: %s' % runCommand
 
 #process = subprocess.Popen([runCommand], shell=True)#), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-'''
+
 #################################################################
 #################################################################
 #################################################################
