e7108b24be20f3a1ab568cccccfc6a4c6609f136
Katie <kmurley@gmail.com>
2020-07-05 00:17:15 +0200

    .

--- composition_uncertainty/write_coreas_t2b.py
+++ composition_uncertainty/write_coreas_t2b.py
@@ -1,114 +1,78 @@
 import numpy as np
-from optparse import OptionParser
+
+proton_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/jobs_proton/'
+helium_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/jobs_helium/'
+oxygen_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/jobs_oxygen/'
+iron_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/jobs_iron/'
+
+base_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/'
+run_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/run_files/'
+
+def write_file(event, type):
+
+
+    part_id=''
+    if type=='proton':
+        part_id='14'
+        outfile=open(proton_dir+event+'_coreas_'+type+'.q','w')
+        
+    if type=='helium':
+        part_id='402'
+        outfile=open(helium_dir+event+'_coreas_'+type+'.q','w')
+
+    if type=='oxygen':
+        part_id='1608'
+        outfile=open(oxygen_dir+event+'_coreas_'+type+'.q','w')
+
+    if type=='iron':
+        part_id='5626'
+        outfile=open(iron_dir+event+'_coreas_'+type+'.q','w')
 
 
 
-proton_dir='/user/kmulrey/energy_systematics/energy_systematics/qgsjet_runs/jobs_proton/'
-iron_dir='/user/kmulrey/energy_systematics/energy_systematics/qgsjet_runs/jobs_iron/'
-base_dir='/user/kmulrey/energy_systematics/pipeline_qgsjet/'
-run_dir='/user/kmulrey/energy_systematics/energy_systematics/run_files/'
 
-event='126484310'
-parser.add_option("-e",--event, type='string', help='event number', default='126484310')
+    inp_dir='/user/kmulrey/energy_systematics/energy_systematics/composition_uncertainty/jobs_'+type+'/'+event+'/'
 
-options, arguments = parser.parse_args()
-event=options.event
+    outfile.write('#! /bin/bash\n')
 
-
-proton_dir='/vol/astro7/lofar/kmulrey/sim/composition_uncertainty/events/events/'+event+'/conex/proton/steering/'
-helium_dir='/vol/astro7/lofar/kmulrey/sim/composition_uncertainty/events/events/'+event+'/conex/helium/steering/'
-oxygen_dir='/vol/astro7/lofar/kmulrey/sim/composition_uncertainty/events/events/'+event+'/conex/oxygen/steering/'
-iron_dir='/vol/astro7/lofar/kmulrey/sim/composition_uncertainty/events/events/'+event+'/conex/iron/steering/'
-
-proton_inp_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/composition_uncertainty/jobs_proton/'+event+'/'
-helium_inp_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/composition_uncertainty/jobs_helium/'+event+'/'
-oxygen_inp_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/composition_uncertainty/jobs_oxygen/'+event+'/'
-iron_inp_dir='/vol/astro3/lofar/sim/kmulrey/energy/LOFARenergy/sim_tests/energy_systematics/composition_uncertainty/jobs_iron/'+event+'/'
-
-
-proton_list=np.genfromtxt('xmax_lists/runs_'+event+'_proton.txt')
-helium_list=np.genfromtxt('xmax_lists/runs_'+event+'_helium.txt')
-oxygen_list=np.genfromtxt('xmax_lists/runs_'+event+'_oxygen.txt')
-iron_list=np.genfromtxt('xmax_lists/runs_'+event+'_iron.txt')
-
-
-for e in np.arange(len(proton_list)):
-    RUNNR=str(int(proton_list[e][0])).zfill(6)
-    proton_filepath=proton_dir+'RUN'+RUNNR+'.inp'
-    outputfile=open(proton_inp_dir+'RUN'+RUNNR+'.inp','w')
-    with open(proton_filepath) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            if 'PAROUT' in line:
-                outputfile.write('PAROUT  T F\n')
-            elif 'CASCADE' in line:
-                outputfile.write('CASCADE F F F\n')
-            else:
-                outputfile.write(line)
-     
-            line = fp.readline()
-            cnt += 1
-
-    outputfile.close()
+    outfile.write('export RUNNR=`printf "%06d" $PBS_ARRAYID`\n')
+    outfile.write('mkdir -p {0}/events/{1}/coreas/{2}/steering/\n'.format(base_dir,event,type))
+    outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
+    outfile.write('mkdir -p /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
+   
+    outfile.write('cp {2}/RUN$RUNNR.inp /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id,inp_dir))
+    outfile.write('cp {2}/SIM.reas /scratch/kmulrey/{0}/{1}/$RUNNR/SIM$RUNNR.reas\n'.format(event,part_id,run_dir))
+    outfile.write('cp {2}/SIM{0}.list /scratch/kmulrey/{0}/{1}/$RUNNR/SIM$RUNNR.reas\n'.format(event,part_id,run_dir))   
+   
+    outfile.write('cd /user/kmulrey/software/corsika-77100/run/\n')
+    outfile.write('./corsika77100Linux_QGSII_urqmd_thin_coreas < //scratch/kmulrey/{0}/{1}/$RUNNR/RUN$RUNNR.inp\n'.format(event,part_id))
+    outfile.write('cd /scratch/kmulrey/{0}/{1}/$RUNNR\n'.format(event,part_id))
+    outfile.write('mv RUN$RUNNR.inp {0}/events/{1}/corsika/{2}/steering/RUN$RUNNR.inp\n'.format(base_dir,event,type))
+    outfile.write('mv *.long {0}events/{1}/corsika/{2}/\n'.format(base_dir,event,type))
     
-for e in np.arange(len(helium_list)):
-    RUNNR=str(int(helium_list[e][0])).zfill(6)
-    helium_filepath=helium_dir+'RUN'+RUNNR+'.inp'
-    outputfile=open(helium_inp_dir+'RUN'+RUNNR+'.inp','w')
-    with open(helium_filepath) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            if 'PAROUT' in line:
-                outputfile.write('PAROUT  T F\n')
-            elif 'CASCADE' in line:
-                outputfile.write('CASCADE F F F\n')
-            else:
-                outputfile.write(line)
-     
-            line = fp.readline()
-            cnt += 1
+    outfile.write('mv SIM$RUNNR.reas {0}events/{1}/coreas/{2}/steering/SIM$RUNNR.reas\n'.format(base_dir,event,type))
+    outfile.write('mv SIM$RUNNR.list {0}events/{1}/coreas/{2}/steering/SIM$RUNNR.list\n'.format(base_dir,event,type))
+    outfile.write('cp -r * {0}events/{1}/corsika/{2}/\n'.format(base_dir,event,type))
+    outfile.write('rm -rf /scratch/kmulrey/{0}/{1}/$RUNNR/*\n'.format(event,part_id))
 
-    outputfile.close()
-
-
-for e in np.arange(len(oxygen_list)):
-    RUNNR=str(int(oxygen_list[e][0])).zfill(6)
-    oxygen_filepath=oxygen_dir+'RUN'+RUNNR+'.inp'
-    outputfile=open(oxygen_inp_dir+'RUN'+RUNNR+'.inp','w')
-    with open(oxygen_filepath) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            if 'PAROUT' in line:
-                outputfile.write('PAROUT  T F\n')
-            elif 'CASCADE' in line:
-                outputfile.write('CASCADE F F F\n')
-            else:
-                outputfile.write(line)
- 
-            line = fp.readline()
-            cnt += 1
-
-    outputfile.close()
+    outfile.close()
     
-for e in np.arange(len(iron_list)):
-    RUNNR=str(int(iron_list[e][0])).zfill(6)
-    iron_filepath=iron_dir+'RUN'+RUNNR+'.inp'
-    outputfile=open(iron_inp_dir+'RUN'+RUNNR+'.inp','w')
-    with open(iron_filepath) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            if 'PAROUT' in line:
-                outputfile.write('PAROUT  T F\n')
-            elif 'CASCADE' in line:
-                outputfile.write('CASCADE F F F\n')
-            else:
-                outputfile.write(line)
-     
-            line = fp.readline()
-            cnt += 1
+    
+   
+   
 
-    outputfile.close()
+
+
+#event=[61254596,95166806,126484310,177295087,]
+#azimuth=[-434.547641748+270,-191.888958177+270,-131.364106846+270,-411.974034426+270]
+#zenith=[21.659887910,35.9479424193,36.4747650732,22.475475965]
+#energy=[91947841.7402,197551979.48,785829352.566,214482624.249]
+#seed=[64704,35764,28210,59522]
+
+event=[95166806,126484310]
+for i in np.arange(len(event)):
+    write_file(str(int(event[i])),'proton')
+    write_file(str(int(event[i])),'helium')
+    write_file(str(int(event[i])),'oxygen')
+    write_file(str(int(event[i])),'iron')
+
