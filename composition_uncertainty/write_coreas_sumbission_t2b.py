64111e6cf8173de991fe68221554c50c3063f568
Katie <kmurley@gmail.com>
2020-07-23 12:28:42 +0200

    .

--- composition_uncertainty/write_coreas_sumbission_t2b.py
+++ composition_uncertainty/write_coreas_sumbission_t2b.py
@@ -38,28 +38,28 @@ for e in np.arange(len(proton_list)-1):
     if countP<(max_runs-1):
         outfile.write('{0},'.format(int(proton_list[countP][0])))
         countP=countP+1
-outfile.write('{0}  {1}_coreas_proton.q'.format(proton_list[countP+1][0],event))
+outfile.write('{0}  {1}_coreas_proton.q\n'.format(proton_list[countP+1][0],event))
 
 
 for e in np.arange(len(helium_list)-1):
     if countHe<(max_runs-1):
         outfile.write('{0},'.format(int(helium_list[countHe][0])))
         countHe=countHe+1
-outfile.write('{0}  {1}_coreas_helium.q'.format(helium_list[countHe+1][0],event))
+outfile.write('{0}  {1}_coreas_helium.q\n'.format(helium_list[countHe+1][0],event))
 
 
 for e in np.arange(len(oxygen_list)-1):
     if countO<(max_runs-1):
         outfile.write('{0},'.format(int(oxygen_list[countO][0])))
         countO=countO+1
-outfile.write('{0}  {1}_coreas_oxygen.q'.format(oxygen_list[countO+1][0],event))
+outfile.write('{0}  {1}_coreas_oxygen.q\n'.format(oxygen_list[countO+1][0],event))
 
 
 for e in np.arange(len(iron_list)-1):
     if countFe<(max_runs-1):
         outfile.write('{0},'.format(int(iron_list[countFe][0])))
         countFe=countFe+1
-outfile.write('{0}  {1}_coreas_iron.q'.format(iron_list[countFe+1][0],event))
+outfile.write('{0}  {1}_coreas_iron.q\n'.format(iron_list[countFe+1][0],event))
 
 
 
